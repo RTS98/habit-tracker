d20e587eb4ed00099a8f5fb8eb000fafe137937e
robertstoia <robertstoia@yahoo.com>
2026-06-04 18:27:54 +0300

    update deploy script

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 import env from "./env.ts";
 
-console.log(
-  "Using migration database URL:",
-  process.env.MIGRATION_DATABASE_URL ?? env.DATABASE_URL,
-);
-
 export default defineConfig({
   out: "./migrations",
   schema: "./src/db/schema.ts",
