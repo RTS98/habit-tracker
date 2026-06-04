58be6dce31757da08f6059e1dff00aa5b6ba0880
robertstoia <robertstoia@yahoo.com>
2026-06-04 19:01:44 +0300

    update url in drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "drizzle-kit";
-import env from "./env.ts";
+
+const url = process.env.MIGRATION_DATABASE_URL;
+
+if (!url) {
+  throw new Error("MIGRATION_DATABASE_URL is not set");
+}
 
 export default defineConfig({
   out: "./migrations",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.MIGRATION_DATABASE_URL ?? env.DATABASE_URL,
-  },
+  dbCredentials: { url },
   migrations: {
     table: "migrations_table",
     schema: "public",
