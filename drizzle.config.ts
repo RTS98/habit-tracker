0fefbf4fe0be7cd8afa33ca629822c4cbe3e0484
robertstoia <robertstoia@yahoo.com>
2026-06-05 07:52:54 +0300

    remove specific params in drizzle migrate

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   out: "./migrations",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
-  dbCredentials: { url },
+  dbCredentials: { url, ssl: false },
   migrations: {
     table: "migrations_table",
     schema: "public",
